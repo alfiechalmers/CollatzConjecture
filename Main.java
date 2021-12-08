ce4580400f35c50b0c30dbbfc0ee272a8ee08194
Alfie Chalmers <alfiechalmers@yahoo.com>
2021-12-08 13:36:26 +0000

    gitignore please
    vscode preview shows blanked out class and csv files, hopefully will work now

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class CollatzConjecture {
+public class Main {
     public static void main (String[] args) {
         try {
             long timeStart = System.currentTimeMillis();
